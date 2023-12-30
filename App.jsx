b491ecb7c2e3730f81d1deb115d995271797d915
yjb23tech <yjb23tech@gmail.com>
2023-12-30 17:44:08 +0000

    Nice and Tidy

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client"
+
+const App = () => {
+    return (
+        <div>
+            <h1>Sanity Check</h1>
+        </div>
+    )
+}
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(<App />)
+
