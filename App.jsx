e0c0d63d0e9e65cfccc38c5adb63e8d899e4f8ae
yjb23tech <yjb23tech@gmail.com>
2023-12-30 19:39:34 +0000

    Next leg up

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import Earth from "./pages/Earth.jsx"
 import Mars from "./pages/Mars.jsx"
@@ -6,6 +7,13 @@ import Mars from "./pages/Mars.jsx"
 const App = () => {
     return (
         <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/earth" element={<Earth />} />
+                    <Route path="/mars" element={<Mars />} />
+                </Routes>
+            </BrowserRouter>
             <Mars />
         </div>
     )
