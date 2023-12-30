36266328324694d16239cc1b376629c8c678ae53
yjb23tech <yjb23tech@gmail.com>
2023-12-30 17:49:41 +0000

    Laughable

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
             <h1>Terra Firma</h1>
             <h2>Mother Gaia</h2>
             <h3>The Blue Planet</h3>
-
+            <p>I been around the World</p>
         </div>
     )
 }
